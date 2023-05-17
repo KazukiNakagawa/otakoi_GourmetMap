eb50bceddb22a02139348b53bc6c2186361dded4
KazukiNakagawa <kazu31292@gmail.com>
2023-05-17 14:53:09 +0900

    app_make

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'home/top'
+  root 'home#top'
+  resource :user, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
