942ef9004bbae109f52042b901e6874840ba0759
KazukiNakagawa <kazu31292@gmail.com>
2023-05-25 16:54:01 +0900

    login_fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
-      redirect_to user_path, notice: "you have successfully sign in"
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "you have successfully sign in"
     else
       render :new
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_22_053345) do
+ActiveRecord::Schema.define(version: 2023_05_24_095804) do
 
   create_table "shops", force: :cascade do |t|
     t.string "name"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2023_05_22_053345) do
     t.string "profile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
