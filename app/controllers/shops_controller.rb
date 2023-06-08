4677d123f4c8309a56ea93a4a5e0a98c2faa73b4
KazukiNakagawa <kazu31292@gmail.com>
2023-06-08 12:12:02 +0900

    fix_2

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,12 +1,8 @@
-#require 'open-uri'
-#require 'JSON'
-
 class ShopsController < ApplicationController
     before_action :set_shop, only: [:show, :edit, :update, :destroy]
   
     def index
       @shops = Shop.all
-      #scrape_instagram("kazuki_na_0312")
     end
   
     def show
@@ -21,6 +17,7 @@ class ShopsController < ApplicationController
     def create
       @shop = Shop.new(shop_params)
       @shop.image.attach(params[:shop][:image]) if params[:shop][:image]
+      byebug
       tag_names = params[:shop][:tag_names].split(",")
 
       if @shop.save
@@ -49,18 +46,6 @@ class ShopsController < ApplicationController
       @shop.destroy
       redirect_to shops_path, notice: '店の情報が削除されました。'
     end
-
-    #def scrape_instagram(user_id)
-      #puts "test"
-      #begin
-        #byebug
-        #instagram_source = open("https://www.instagram.com/#{user_id}").read
-        #content = JSON.parse(instagram_source.split("window._sharedData = ")[1].split(";</script>")[0])
-        #return content['entry_data']['ProfilePage'][0]['user']
-      #rescue Exception => e
-        #return nil
-      #end
-    #end  
   
     private
   
