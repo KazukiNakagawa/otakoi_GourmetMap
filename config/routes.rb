8a32876dba2c7ad55325d0ffa48b410c37587344
KazukiNakagawa <kazu31292@gmail.com>
2023-06-13 15:04:31 +0900

    comment_make

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     member do
       get 'reviews'
     end
+    resources :comments, only: [:create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
