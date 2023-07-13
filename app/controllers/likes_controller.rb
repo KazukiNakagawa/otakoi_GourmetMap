97d17b5c1c7c6390ba929a17210b27caa6dda3ff
KazukiNakagawa <kazu31292@gmail.com>
2023-07-13 15:34:28 +0900

    like_fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,19 @@
 class LikesController < ApplicationController
-    #before_action :authenticate_user!, only: [:create]
-  
-    def create
-      @shop = Shop.find(params[:id])
-      current_user.likes.create(shop: @shop)
-      redirect_back fallback_location: root_path, notice: 'いいねしました。'
-    end
-  
-    def destroy
-      @shop = Shop.find(params[:id])
-      current_user.likes.find_by(shop: @shop).destroy
-      redirect_back fallback_location: root_path, notice: 'いいねを解除しました。'
-    end
+  #before_action :authenticate_user!, only: [:create]
+  def index
+    @liked_shops = current_user.likes.map(&:shop)
   end
+  
+  def create
+    @shop = Shop.find(params[:id])
+    current_user.likes.create(shop: @shop)
+    redirect_back fallback_location: root_path, notice: 'いいねしました。'
+  end
+  
+  def destroy
+    @shop = Shop.find(params[:id])
+    current_user.likes.find_by(shop: @shop).destroy
+    redirect_back fallback_location: root_path, notice: 'いいねを解除しました。'
+  end
+end
   
