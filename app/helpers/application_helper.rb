77df122c56319bb120e398c0429368fa99b2d343
KazukiNakagawa <kazu31292@gmail.com>
2023-07-21 19:26:00 +0900

    googleMap_add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def link_hashtags(text)
     text.gsub(/#(\w+)/) { |match| link_to(match, hashtag_path($1)) }
   end
+
+  def google_maps_link(address)
+    "https://www.google.com/maps/search/#{CGI.escape(address)}"
+  end
 end
